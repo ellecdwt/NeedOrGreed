7b7f87430ee39c335b823ef37950a1e214f7cedf
ellecdwt <ldewitt@pdx.edu>
2014-07-06 14:25:24 -0700

    Updated lua and toc
    Updated the toc with more metadata and changed the hello world lua
    function to an event handling function that keeps track of how many
    logins a user has on each character. This is part of figuring out how
    lua works with events in the addon

--- NeedOrGreed.lua
+++ NeedOrGreed.lua
@@ -1,3 +1,15 @@
-function HelloWorld()
-  print("Hello, World!");
-end
+local EventFrame = CreateFrame("Frame")
+EventFrame:RegisterEvent("PLAYER_LOGIN")
+EventFrame:SetScript("OnEvent", function(self,event,...) 
+	if type(NeedOrGreedPerCharDB) ~= "number" then
+		NeedOrGreedPerCharDB = 1
+		ChatFrame1:AddMessage('Hello, '.. UnitName("Player")..". Welcome to WoW!")
+	else
+		if NeedOrGreedPerCharDB == 1 then
+			ChatFrame1:AddMessage('Hello, '.. UnitName("Player")..". You have logged in " .. NeedOrGreedPerCharDB .. " time before.")
+		else
+			ChatFrame1:AddMessage('Hello '.. UnitName("Player")..". You have logged in " .. NeedOrGreedPerCharDB .. " times before.")
+		end
+		NeedOrGreedPerCharDB = NeedOrGreedPerCharDB + 1
+	end
+end)
