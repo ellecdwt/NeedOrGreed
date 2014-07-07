e98e4f405fee1a443cc7d34f8fbd6ffe4939b2b3
ellecdwt <ldewitt@pdx.edu>
2014-07-06 21:09:01 -0700

    Added Title and description to config
    Added the title and description to the interface options frame

--- Config.lua
+++ Config.lua
@@ -1,8 +1,17 @@
 -- Create main frame for information text
-local EventFrame = CreateFrame("Frame")
-EventFrame:RegisterEvent("PLAYER_LOGIN")
-EventFrame:SetScript("OnEvent", function(self,event,...) 
-	local panel = CreateFrame("FRAME")
-	panel.name = GetAddOnMetadata("NeedOrGreed", "Title")
-	InterfaceOptions_AddCategory(panel)
-end)
+local panel = CreateFrame("FRAME")
+panel.name = GetAddOnMetadata("NeedOrGreed", "Title")
+
+local panel_title = panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+panel_title:SetPoint("TOPLEFT", 16, -16)
+panel_title:SetText(GetAddOnMetadata("NeedOrGreed", "Title"))
+
+local panel_subtext = panel:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+panel_subtext:SetPoint("TOPLEFT", panel_title, "BOTTOMLEFT", 0, -8)
+panel_subtext:SetPoint("RIGHT", -32, 0)
+panel_subtext:SetHeight(32)
+panel_subtext:SetJustifyH("LEFT")
+panel_subtext:SetJustifyV("TOP")
+panel_subtext:SetText(GetAddOnMetadata("NeedOrGreed", "Notes"))
+
+InterfaceOptions_AddCategory(panel)
