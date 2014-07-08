2f94472bb785e2d9e4949a69eb7a1a21859c7c0a
ellecdwt <ldewitt@pdx.edu>
2014-07-07 19:05:30 -0700

    table testing
    Added a test for storing nested tables, which will hold the recipes, and
    their required reagents.

---
+++ TableTest.lua
@@ -0,0 +1,7 @@
+alchemy = {}
+
+alchemy["Vial of the Sands"] = {"Pyrium-Laced Crystalline Vial", "Sands of Time", "Truegold", "Flask of the Winds", "Flask of Titanic Strength", "Deepstone Oil"}
+
+--  table.insert(alchemy, "Vial of the Sands" = {"Pyrium-Laced Crystalline Vial", "Sands of Time", "Truegold", "Flask of the Winds", "Flask of Titanic Strength", "Deepstone Oil"})
+  
+  print(alchemy["Vial of the Sands"][2])
